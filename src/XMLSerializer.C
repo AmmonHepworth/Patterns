a2c1cf5e6b90d1b5a78e2f8ba6c49a7c58a3f78c
Ammon Hepworth <ammonhepworth@outook.com>
2020-10-12 22:13:56 -0400

    finish template method

--- src/XMLSerializer.C
+++ src/XMLSerializer.C
@@ -5,22 +5,17 @@
 #include "Attr.H"
 #include "Text.H"
 
-void XMLSerializer::prettyIndentation()
-{
-	for (int i = 0; i < indentationLevel; i++)
-		file << "\t";
-}
-
-void XMLSerializer::serializePretty(dom::Node * node)
+void XMLSerializer::serialize(dom::Node * node)
 {
 	if (dynamic_cast<dom::Document *>(node) != 0)
 	{
-		file << "<? xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		file << "\n";
-		serializePretty(dynamic_cast<dom::Document *>(node)->getDocumentElement());
+		serializeDocument(node); 
+		file << getLineEnding();
+		serialize(dynamic_cast<dom::Document *>(node)->getDocumentElement());
 	}
 	else if (dynamic_cast<dom::Element *>(node) != 0)
 	{
+
 		prettyIndentation();
 		file << "<" << dynamic_cast<dom::Element *>(node)->getTagName();
 
@@ -30,7 +25,7 @@ void XMLSerializer::serializePretty(dom::Node * node)
 		  i != dynamic_cast<dom::Element *>(node)->getAttributes()->end();
 		  i++)
 		{
-			serializePretty(*i);
+			serialize(*i);
 			attrCount++;
 		}
 
@@ -40,75 +35,84 @@ void XMLSerializer::serializePretty(dom::Node * node)
 		if (dynamic_cast<dom::Element *>(node)->getChildNodes()->size() == 0)
 		{
 			file << "/>";
-			file << "\n";
+			file << getLineEnding();
 		}
 		else
 		{
 			file << ">";
-			file << "\n";
+			file << getLineEnding();
 			indentationLevel++;
 
 			for (dom::NodeList::iterator i = dynamic_cast<dom::Element *>(node)->getChildNodes()->begin();
 			  i != dynamic_cast<dom::Element *>(node)->getChildNodes()->end();
 			  i++)
 				if (dynamic_cast<dom::Element *>(*i) != 0 || dynamic_cast<dom::Text *>(*i) != 0)
-					serializePretty(*i);
+					serialize(*i);
 
 			indentationLevel--;
 			prettyIndentation();
 			file << "</" << dynamic_cast<dom::Element *>(node)->getTagName() + ">";
-			file << "\n";
+			file << getLineEnding();
 		}
 	}
 	else if (dynamic_cast<dom::Attr *>(node) != 0)
 	{
-		file <<
-		  " " << dynamic_cast<dom::Attr *>(node)->getName() << "=\"" << dynamic_cast<dom::Attr *>(node)->getValue() << "\"";
+		  serializeAttribute(node);
 	}
 	else if (dynamic_cast<dom::Text *>(node) != 0)
 	{
 		prettyIndentation();
-		file << dynamic_cast<dom::Text *>(node)->getData();
-		file << "\n";
+		serializeText(node);
+		file << getLineEnding();
 	}
 }
 
-void XMLSerializer::serializeMinimal(dom::Node * node)
+void PrettyXMLSerializer::serializeDocument(dom::Node * node)
 {
-	if (dynamic_cast<dom::Document *>(node) != 0)
-	{
-		file << "<? xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		serializeMinimal(dynamic_cast<dom::Document *>(node)->getDocumentElement());
-	}
-	else if (dynamic_cast<dom::Element *>(node) != 0)
-	{
-		file << "<" << dynamic_cast<dom::Element *>(node)->getTagName();
+	file << "<? xml version=\"1.0\" encoding=\"UTF-8\"?>";
+}
 
-		for (dom::NamedNodeMap::iterator i = dynamic_cast<dom::Element *>(node)->getAttributes()->begin();
-		  i != dynamic_cast<dom::Element *>(node)->getAttributes()->end();
-		  i++)
-			serializeMinimal(*i);
+void PrettyXMLSerializer::serializeAttribute(dom::Node * node)
+{
+	file << " " << dynamic_cast<dom::Attr *>(node)->getName() << "=\"" << dynamic_cast<dom::Attr *>(node)->getValue() << "\"";
+}
+void PrettyXMLSerializer::serializeText(dom::Node * node)
+{
+	file << dynamic_cast<dom::Text *>(node)->getData();
+}
 
-		if (dynamic_cast<dom::Element *>(node)->getChildNodes()->size() == 0)
-			file << "/>";
-		else
-		{
-			file << ">";
+void MinimalXMLSerializer::serializeDocument(dom::Node * node)
+{
+	file << "<? xml version=\"1.0\" encoding=\"UTF-8\"?>";
+}
 
-			for (dom::NodeList::iterator i = dynamic_cast<dom::Element *>(node)->getChildNodes()->begin();
-			  i != dynamic_cast<dom::Element *>(node)->getChildNodes()->end();
-			  i++)
-				if (dynamic_cast<dom::Element *>(*i) != 0 || dynamic_cast<dom::Text *>(*i) != 0)
-					serializeMinimal(*i);
+void MinimalXMLSerializer::serializeAttribute(dom::Node * node)
+{
+	file << " " << dynamic_cast<dom::Attr *>(node)->getName() << "=\"" << dynamic_cast<dom::Attr *>(node)->getValue() << "\"";
+}
+void MinimalXMLSerializer::serializeText(dom::Node * node)
+{
+	file << dynamic_cast<dom::Text *>(node)->getData();
+}
 
-			file << "</" << dynamic_cast<dom::Element *>(node)->getTagName() + ">";
-		}
-	}
-	else if (dynamic_cast<dom::Attr *>(node) != 0)
-	{
-		file <<
-		  " " << dynamic_cast<dom::Attr *>(node)->getName() << "=\"" << dynamic_cast<dom::Attr *>(node)->getValue() << "\"";
-	}
-	else if (dynamic_cast<dom::Text *>(node) != 0)
-		file << dynamic_cast<dom::Text *>(node)->getData();
+
+void PrettyXMLSerializer::prettyIndentation()
+{
+	for (int i = 0; i < indentationLevel; i++)
+		file << "\t";
+}
+
+void MinimalXMLSerializer::prettyIndentation()
+{
+	return;
+}
+
+std::string PrettyXMLSerializer::getLineEnding()
+{
+	return "\n";
+}
+
+std::string MinimalXMLSerializer::getLineEnding()
+{
+	return "";
 }
